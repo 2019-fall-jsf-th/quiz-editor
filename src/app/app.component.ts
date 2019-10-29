ff7f15cc303e59ec5f4ff06cae14dd2eb781fa78
Tom Steele <tsteele@madisoncollege.edu>
2019-10-29 15:16:05 -0500

    Messing with property and style binding...
    Bootstrap for class binding examples next...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+
+  myBackgroundColorProperty = "purple";
+  
+  ngOnInit() {
+    this.myBackgroundColorProperty = Math.random() > 0.5 ? 'yellow' : 'green';
+  }
+
+  get titleTooltip() {
+    return `The background color is ${this.myBackgroundColorProperty}`;
+  }
 }
