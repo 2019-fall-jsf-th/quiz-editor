2bd0bc8d16f3e5d29bac2773c0d823be500e0a2e
tom13527 <47016464+tom13527@users.noreply.github.com>
2019-10-31 19:03:29 -0500

    Other bindings....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  private random = Math.random();
+  propName = this.random > 0.5 ? 'Green' : 'Yellow';
+  borderRadius = this.random > 0.5 ? '30px' : '0px';
 
-  propName = Math.random() > 0.5 ? 'Green' : 'Yellow';
-  borderRadius = Math.random() > 0.5 ? '30px' : '0px';
+  toolTipText = `The color is ${this.propName} ${this.random}`;
+
+  someHtmlString = '<h1>Tom Good</h1>';
 }
