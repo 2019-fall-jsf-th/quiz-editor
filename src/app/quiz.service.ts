406617c639de1dcb93a3ca79b1d4cca1a24b0c00
caheilman <cheilman920@gmail.com>
2019-11-07 19:38:20 -0600

    oops

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private httpSvc: HttpClient) { }
 
   loadQuizzes() {
 
-    return [
-      { name: "Quiz 1", questionCount: 10 }
-      , { name: "Quiz 2", questionCount: 0 }
-      , { name: "Quiz 3", questionCount: 25 }
-    ];
+    return this.builtInAngularHttpClient.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz';)
+
+    // return [
+    //   { name: "Quiz 1", questionCount: 10 }
+    //   , { name: "Quiz 2", questionCount: 0 }
+    //   , { name: "Quiz 3", questionCount: 25 }
+    // ];
   }
 }
