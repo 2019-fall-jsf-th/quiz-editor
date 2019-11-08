abe3e478a37ecfa481e6579b926e3f46246c7bc9
TimErr <44173161+TimErr@users.noreply.github.com>
2019-11-07 20:02:42 -0600

    Load Quizzes from Cloud

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   loadQuizzes() {
 
-    return [
-      { name: "Quiz 1", questionCount: 10 }
-      , { name: "Quiz 2", questionCount: 0 }
-      , { name: "Quiz 3", questionCount: 25 }
-    ];
+    return this.builtInAngularHttpClient.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Tim%20Errickson');
+
+    // return [
+    //   { name: "Quiz 1", questionCount: 10 }
+    //   , { name: "Quiz 2", questionCount: 0 }
+    //   , { name: "Quiz 3", questionCount: 25 }
+    // ];
   }
 }
