6bc41b0eaff021d7e66137c2edbd33e97528cae6
jpsweeney94 <jpsweeney94@icloud.com>
2019-11-07 20:02:45 -0600

    Load quizzes from cloud

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   loadQuizzes() {
+    return this.builtInAngularHttpClient.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Joe%20Sweeney');
 
-    return [
-      { name: "Quiz 1", questionCount: 10 }
-      , { name: "Quiz 2", questionCount: 0 }
-      , { name: "Quiz 3", questionCount: 25 }
-    ];
+    // return [
+    //   { name: "Quiz 1", questionCount: 10 }
+    //   , { name: "Quiz 2", questionCount: 0 }
+    //   , { name: "Quiz 3", questionCount: 25 }
+    // ];
   }
 }
