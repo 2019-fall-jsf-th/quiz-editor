2347b105f8f27b5c0deecb606502775846d75b4e
gitrlyn <54748346+gitrlyn@users.noreply.github.com>
2019-11-12 23:25:59 -0600

    trying to get new question functionallity to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,6 +77,6 @@ export class AppComponent implements OnInit {
   }
 
   addNewQuestion() {
-    
+    this.selectedQuiz.questions = [...this.selectedQuiz.questions, { name: "New question"}];
   }
 }
