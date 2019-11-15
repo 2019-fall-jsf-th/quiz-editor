e1188506b1510adc1f3f6d40633f6964b99d7ff2
corin920 <cheilman920@mail.com>
2019-11-14 18:35:52 -0600

    promise was made
    Then we intentionally broke it in the app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -89,4 +89,22 @@ export class AppComponent implements OnInit {
   removeQuestion(question) {
     this.selectedQuiz.questions = this.selectedQuiz.questions.filter(x => x !== question);
   }
+
+  jsPromisesOne() {
+    const x = this.qSvc.getMagicNumberPromise(true);
+    console.log(x);
+
+    x.then(
+      n => {
+        console.log(n);
+
+        const y = this.qSvc.getMagicNumberPromise(false);
+        console.log(y);
+
+        y.then( n => { console.log(n); } )
+        .catch(err => console.log(err))
+      }
+    )
+    .catch(err => console.log(err))
+  }
 }
