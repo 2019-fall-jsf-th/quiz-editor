54fb14c0c5181e2f296334c7fe2c3ad172fc8d27
gitrlyn <54748346+gitrlyn@users.noreply.github.com>
2019-11-20 20:46:26 -0600

    imported ng-bootstrap
    use command line npm install --save @ng-bootstrap/ng-bootstrap and added import statements to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
