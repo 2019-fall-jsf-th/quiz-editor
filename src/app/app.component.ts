00254e7a480bbab5f3eb2776859924a053977f97
jpsweeney94 <jpsweeney94@icloud.com>
2019-11-21 19:56:13 -0600

    saving edited quizzes
    Not really...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -188,5 +188,16 @@ export class AppComponent implements OnInit {
         && !x.markedForDelete
       );
   }
+
+  saveBatchEdits() {
+    this.qSvc.saveQuizzes(
+      this.getEditedQuizzes(),
+      []
+    )
+    .subscribe(
+      data => console.log('Number of edited quizzes submitted: ' + data)
+      , err => console.log(err)
+    );
+  }
   
 }
